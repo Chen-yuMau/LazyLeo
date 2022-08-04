0c5445a3a93643a6d7d4db4fce0efd3fdad56832
brianmau0826@gmail.com <brianmau0826@gmail.com>
2022-08-04 05:03:17 -0400

    Decided on new game rules
    We decided to create a platformer game where the player needs to move by the beat. Inspired by "Turn Undead" and "Crypt of the Necrodancer"

--- Objects/Player.gd
+++ Objects/Player.gd
@@ -1,39 +1,74 @@
 extends KinematicBody2D
 
 onready var screen_shaker = $Camera2D/screen_shaker
-var move = Vector2()
 var speed = 200
 var jump_speed = -400
 var grv = 10
 var last_dir = 1
-
+var pos
+var bps = 2.0
+var k = 0
+var onbeat = false
+var wasonbeat = false
+var wasoffbeat = true
+var move = 'n'
+var offground = 0
 func _physics_process(delta):
-	if is_on_floor():
-		move.y = 0 
-		if move.x != 0:
-			$Sprite.play("Run")
-		else:
-			$Sprite.play("Idle")
+	if onbeat == true:
+		wasoffbeat = false
+		wasonbeat = true
 	else:
-		move.y += grv
-		if move.y>11:
-			$Sprite.play("Fall")
-		if move.y<0:
-			$Sprite.play("Jump")
-	if is_on_ceiling():
-		move.y = 1
+		wasoffbeat = true
+		wasonbeat = false
+	k+=delta
+	if k>(1/bps):
+		k = 0
+		$AudioStreamPlayer2D.play()
+	if k < 0.35*(1/bps) or k > 0.8*(1/bps):
+		onbeat = true
+	else:	
+		onbeat = false
 		
-	if sign(move.x) == (0):
-		$Sprite.flip_h= last_dir == (-1)
-	elif sign(move.x) == (-1):
-		last_dir = -1
-		$Sprite.flip_h=true
-	else:
-		last_dir = 1
-		$Sprite.flip_h=false
+	if wasoffbeat and onbeat:
+		move = 'n'
+	if onbeat and Input.is_action_pressed("ui_left"):
+		move = 'l'
+	if onbeat and Input.is_action_pressed("ui_right"):
+		move = 'r'
+	if onbeat and Input.is_action_pressed("ui_up"):
+		move = 'u'
+	if onbeat and Input.is_action_pressed("ui_down"):
+		move = 'd'
 	
-	move.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))*speed
-	print(move.y)
-	if Input.is_action_pressed("ui_up")and is_on_floor():
-		move.y = jump_speed
-	move_and_slide(move, Vector2.UP)
+	if not onbeat and Input.is_action_pressed("ui_left"):
+		if k>0.5:
+			print("E")
+		else:
+			print("L")
+	if not onbeat and Input.is_action_pressed("ui_right"):
+		if k>0.5:
+			print("E")
+		else:
+			print("L")
+	if not onbeat and Input.is_action_pressed("ui_up"):
+		if k>0.5:
+			print("E")
+		else:
+			print("L")
+	if not onbeat and Input.is_action_pressed("ui_down"):
+		if k>0.5:
+			print("E")
+		else:
+			print("L")
+	
+	if wasonbeat and not onbeat:
+		if move == 'l':
+			self.global_position+=Vector2(-32,0)
+		if move == 'r':
+			self.global_position+=Vector2(32,0)
+		if move == 'u':
+			self.global_position+=Vector2(0,-32)
+		if move == 'd':
+			self.global_position+=Vector2(0,32)
+	
+
