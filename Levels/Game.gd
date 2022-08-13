fcee4747b39ca97c3644a5b71e0ad4cb10daaf8a
brianmau0826@gmail.com <brianmau0826@gmail.com>
2022-08-13 07:01:56 -0400

    Main Game Mechanic
    Added Main game mechanic

--- Levels/Game.gd
+++ Levels/Game.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+onready var camera = $Camera2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -9,9 +10,19 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
-var k = 0
 var bps = 2.0
+var cam_pan_overlap = 3
+var cam_slide_width = 1024-cam_pan_overlap*32
+var cam_position_number = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
+func slide_camera(D):
+	if D == 'l':
+		if cam_position_number !=0:
+			cam_position_number-=1
+			camera.global_position = camera.global_position+Vector2(-cam_slide_width,0)			
+	if D == 'r':
+		cam_position_number+=1
+		camera.global_position = camera.global_position+Vector2(cam_slide_width,0)
+	
--- Objects/Player.gd
+++ Objects/Player.gd
@@ -1,19 +1,41 @@
 extends KinematicBody2D
 
 onready var screen_shaker = $Camera2D/screen_shaker
-var speed = 200
-var jump_speed = -400
-var grv = 10
 var last_dir = 1
-var pos
+var forgiveness_radius = 0.2 #Cannot be more than 0.5
 var bps = 2.0
+var blt = (1-forgiveness_radius)*(1/bps)
+var bht = forgiveness_radius*(1/bps)
+var beatlowerthreshold = blt
+var beathigherthreshold = bht
 var k = 0
 var onbeat = false
 var wasonbeat = false
 var wasoffbeat = true
 var move = 'n'
 var offground = 0
+var detect
+var inair = 0
+var avdelay = 0
+var currentdelay = 0
+var delaynum = 10
+var delay = []
+onready var pos_ontile = get_parent().world_to_map(self.global_position)
+onready var under = pos_ontile+Vector2(0,1)
+onready var left = pos_ontile+Vector2(-1,0)
+onready var right = pos_ontile+Vector2(1,0)
+
+func has_tile(T):
+	return(get_parent().get_cellv(T)!=(-1))
 func _physics_process(delta):
+	$Sprite.flip_h = (last_dir == -1)
+	#get surrounding tile coordinates
+	pos_ontile = get_parent().world_to_map(self.global_position)
+	under = pos_ontile+Vector2(0,1)
+	left = pos_ontile+Vector2(-1,0)
+	right = pos_ontile+Vector2(1,0)
+	
+	#determine timing of current frame(in input window or not)
 	if onbeat == true:
 		wasoffbeat = false
 		wasonbeat = true
@@ -24,51 +46,110 @@ func _physics_process(delta):
 	if k>(1/bps):
 		k = 0
 		$AudioStreamPlayer2D.play()
-	if k < 0.35*(1/bps) or k > 0.8*(1/bps):
+	#beatlowerthreshold = (blt)+avdelay
+	while beatlowerthreshold > (1/bps):
+		beatlowerthreshold-=(1/bps)
+	#beathigherthreshold = (bht)+avdelay
+	while beathigherthreshold > (1/bps):
+		beathigherthreshold-=(1/bps)
+	#print((beathigherthreshold+(1/bps)-beatlowerthreshold)-(2*forgiveness_radius*(1/bps)))
+	#print("%f/%f/%f/%f"%[beathigherthreshold+(1/bps)-beatlowerthreshold,beathigherthreshold,beatlowerthreshold,avdelay])
+	#if (beathigherthreshold+(1/bps)-beatlowerthreshold)!=(2*forgiveness_radius*(1/bps)):
+	#	 beatlowerthreshold = blt
+	#	 beathigherthreshold = bht
+	if k > beatlowerthreshold or k < beathigherthreshold:
 		onbeat = true
 	else:	
 		onbeat = false
-		
+	
+	#if in input window
 	if wasoffbeat and onbeat:
 		move = 'n'
 	if onbeat and Input.is_action_pressed("ui_left"):
 		move = 'l'
+		currentdelay = k
 	if onbeat and Input.is_action_pressed("ui_right"):
 		move = 'r'
+		currentdelay = k
 	if onbeat and Input.is_action_pressed("ui_up"):
 		move = 'u'
+		currentdelay = k
 	if onbeat and Input.is_action_pressed("ui_down"):
 		move = 'd'
-	
+		currentdelay = k
+	#display input correlation to beat
 	if not onbeat and Input.is_action_pressed("ui_left"):
+		currentdelay = k
 		if k>0.5:
 			print("E")
 		else:
 			print("L")
 	if not onbeat and Input.is_action_pressed("ui_right"):
+		currentdelay = k
 		if k>0.5:
 			print("E")
 		else:
 			print("L")
 	if not onbeat and Input.is_action_pressed("ui_up"):
+		currentdelay = k
 		if k>0.5:
 			print("E")
 		else:
 			print("L")
 	if not onbeat and Input.is_action_pressed("ui_down"):
+		currentdelay = k
 		if k>0.5:
 			print("E")
 		else:
 			print("L")
 	
+	#immediately after input window over, do input
+	#print("%f/%f/%f"%[blt*bps,avdelay*bps,bht*bps])
+	#print("hey%f"%[currentdelay])
 	if wasonbeat and not onbeat:
-		if move == 'l':
+		#if currentdelay!=0:
+		#	if delay.size()>delaynum:
+		#		delay.pop_front()
+		#	delay.push_back(currentdelay)
+		#	currentdelay = 0
+		#	var sum = 0
+		#	for n in delay:
+		#		sum +=n
+		#	avdelay=sum/delay.size()		
+			#print(avdelay)
+		if move == 'n':
+			inair = 3
+		if move == 'l' and not has_tile(left):
 			self.global_position+=Vector2(-32,0)
-		if move == 'r':
+			under+=Vector2(-1,0)
+			last_dir =  -1
+			if not has_tile(under) and inair<2:
+				inair = 2
+		if move == 'r'and not has_tile(right):
 			self.global_position+=Vector2(32,0)
-		if move == 'u':
+			under+=Vector2(1,0)
+			last_dir =  1
+			if not has_tile(under) and inair<2:
+				inair = 2
+		if move == 'u' and inair<3:
 			self.global_position+=Vector2(0,-32)
-		if move == 'd':
+			under+=Vector2(0,-1)
+		if move == 'd'and not has_tile(under):
 			self.global_position+=Vector2(0,32)
-	
-
+			under+=Vector2(0,1)
+			
+		if not has_tile(under):
+			inair+=1
+		else:
+			inair = 0
+		if inair>3:
+			self.global_position+=Vector2(0,32)
+		var campos = get_parent().get_parent().cam_position_number
+		var campano = get_parent().get_parent().cam_pan_overlap
+		var camslidew = get_parent().get_parent().cam_slide_width
+		if self.global_position.x<campos*camslidew+(campano-1)*32:
+			if last_dir == -1:
+				get_parent().get_parent().slide_camera('l')
+		if (campos+1)*camslidew+32<self.global_position.x :
+			if last_dir == 1:
+				get_parent().get_parent().slide_camera('r')
